fd5cd74b6dbdd6578e6e73f343bad02762c1e32c
Yuri <yuriseki@gmail.com>
2025-12-26 10:21:07 -0300

    Update DAP-UI buttons order.

--- nvim/.config/nvim/lua/plugins/dap-ui.lua
+++ nvim/.config/nvim/lua/plugins/dap-ui.lua
@@ -68,10 +68,61 @@ return {
       -- ---------------------------------------------------------
       opts.listeners = {
         dap = {
-          started = function() end,  -- Disable auto-open on debug start
-          terminated = function() end,  -- Disable auto-close on debug end
+          started = function() end, -- Disable auto-open on debug start
+          terminated = function() end, -- Disable auto-close on debug end
         },
       }
+
+      -- ---------------------------------------------------------
+      -- Override controls order
+      -- ---------------------------------------------------------
+      local controls = require("dapui.controls")
+      controls.controls = function(is_active)
+        local session = dap.session()
+
+        local running = (session and not session.stopped_thread_id)
+
+        local avail_hl = function(group, allow_running)
+          if not session or (not allow_running and running) then
+            return is_active and "DapUIUnavailable" or "DapUIUnavailableNC"
+          end
+          return group
+        end
+
+        local icons = opts.controls and opts.controls.icons
+          or {
+            pause = "",
+            play = "",
+            step_into = "",
+            step_over = "",
+            step_out = "",
+            step_back = "",
+            run_last = "",
+            terminate = "",
+            disconnect = "",
+          }
+        -- Reorder elems: terminate first, then play/pause, step_over, step_into, step_out, step_back, run_last, disconnect
+
+        local elems = {
+          {
+            func = "play",
+            icon = running and icons.pause or icons.play,
+            hl = is_active and "DapUIPlayPause" or "DapUIPlayPauseNC",
+          },
+          { func = "step_over", hl = avail_hl(is_active and "DapUIStepOver" or "DapUIStepOverNC") },
+          { func = "step_into", hl = avail_hl(is_active and "DapUIStepInto" or "DapUIStepIntoNC") },
+          { func = "step_out", hl = avail_hl(is_active and "DapUIStepOut" or "DapUIStepOutNC") },
+          { func = "step_back", hl = avail_hl(is_active and "DapUIStepBack" or "DapUIStepBackNC") },
+          { func = "run_last", hl = is_active and "DapUIRestart" or "DapUIRestartNC" },
+          { func = "terminate", hl = avail_hl(is_active and "DapUIStop" or "DapUIStopNC", true) },
+          { func = "disconnect", hl = avail_hl(is_active and "DapUIStop" or "DapUIStopNC", true) },
+        }
+        local bar = ""
+        for _, elem in ipairs(elems) do
+          bar = bar .. ("  %%#%s#%%0@v:lua._dapui.%s@%s%%#0#"):format(elem.hl, elem.func, elem.icon or icons[elem.func])
+        end
+        return bar
+      end
 
       -- ---------------------------------------------------------
       -- Configure Layout
@@ -89,8 +140,8 @@ return {
         },
         {
           elements = {
-            "scopes",
-            "repl",
+            { id = "repl", size = 0.25 },
+            { id = "scopes", size = 0.75 },
           },
           size = 10,
           position = "bottom", -- Can be "bottom" or "top"
